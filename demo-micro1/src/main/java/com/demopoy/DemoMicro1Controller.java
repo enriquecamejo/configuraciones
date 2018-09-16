43b78c888affdcb639f99753d2d87808d3038ef3
Quique <Quique@192.168.180.1>
2018-09-16 19:03:35 -0300

    a

--- demo-micro1/src/main/java/com/demopoy/DemoMicro1Controller.java
+++ demo-micro1/src/main/java/com/demopoy/DemoMicro1Controller.java
@@ -3,19 +3,19 @@ package com.demopoy;
 //import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class DemoMicro1Controller {
 
-	//@Value("${solucion.id}")
+	@Value("${solucion.id}")
     private String idSI;
 	
 	//protected Logger logger = Logger.getLogger(DemoMicro1Controller.class.getName());
 	
-	@GetMapping("/solucion")	
+	@RequestMapping("/solucion")	
 	public String parametros() {
 		
 		System.out.printf("valor SI: %s",idSI);
